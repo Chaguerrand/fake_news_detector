8cadc81fd49a62fe7068c1fff007df9f8489ebcf
charles jego <charles.jego@gmail.com>
2026-04-20 11:54:30 +0200

    init dossiers

---
+++ streamlit/app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import requests
+import os
+
+#params
+
+API_URL = os.gentenv(localhost)
+
+API_READY = False #true quand c'est good
+
+st.set_page_config(
+    page_title="Fake News Detector",
+    page_icon="📰",
+    layout="centered"
+)
+
+#css pour real/fake
+    #a voir a la fin
+
+#header
+st.title("📰 Fake News Detector 📰")
+st.caption("Détection et catégorisation de Fake News - Le Wagon #2251")
+st.divider()
+
+#saisie
+
+
+#bouton
+
+
+#analyse
+
+
+#footer
+st.divider()
+st.caption("Martin Cornud - Alex Delrieu - Charles Jégo")
+st.caption("Le Wagon - #2251")
